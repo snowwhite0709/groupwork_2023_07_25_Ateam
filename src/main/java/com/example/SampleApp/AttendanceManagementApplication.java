2379bbbc24cda66020c5036497ed74eecd063d4d
3030670 <3030670@30306-70>
2023-12-12 10:41:05 +0900

    test

--- src/main/java/com/example/SampleApp/AttendanceManagementApplication.java
+++ src/main/java/com/example/SampleApp/AttendanceManagementApplication.java
@@ -9,5 +9,5 @@ public class AttendanceManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceManagementApplication.class, args);
 	}
-
+	/*aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa*/
 }
