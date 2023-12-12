bc0181a072e75a9e7d430fa260825197484aa446
3030823 <3030823@30308-23>
2023-12-12 13:58:52 +0900

    戻した

--- src/main/java/com/example/attendanceManagement/repository/LoginUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.attendanceManagement.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.attendanceManagement.entity.LoginUser;
-
-public interface LoginUserRepository extends CrudRepository<LoginUser, String> {
-}
-
--- src/main/java/com/example/attendanceManagement/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.attendanceManagement.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.attendanceManagement.entity.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Integer> {
-}
