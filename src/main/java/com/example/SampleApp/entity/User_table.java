7a318d3d503435db3da49cedf5f2d0aec96bc966
gachauetera <153568939+gachauetera@users.noreply.github.com>
2023-12-12 11:01:59 +0900

    Create User_table.java

---
+++ src/main/java/com/example/SampleApp/entity/User_table.java
@@ -0,0 +1,24 @@
+package com.example.SampleApp.entity;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data               // Getter,Setterが不要になる
+@NoArgsConstructor  // デフォルトコンストラクターの自動生成
+@AllArgsConstructor // 全フィールドに対する初期化値を引数に取るコンストラクタを自動生成
+public class User_table {
+	 @Id
+	private Integer id;
+	private String pass;
+	private String lastname;
+	private String firstname;
+	private Integer sex;
+	private Integer age;
+	private Integer integer;
+	private Integer rank;
+	private Integer admin;
+	
+}
