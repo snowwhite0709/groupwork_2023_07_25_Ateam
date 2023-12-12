880a868af238d571eafd0b204319c834ee8359cb
3030823 <3030823@30308-23>
2023-12-12 15:26:37 +0900

    htmlの更新

--- src/main/java/com/example/attendanceManagement/controller/LoginController.java
+++ src/main/java/com/example/attendanceManagement/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.attendanceManagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,13 +11,13 @@ public class LoginController {
 		return "login";
 	}
 
-//	// SecurityConfig の failureUrl で指定した URL と?のうしろのパラメータ
-//	@GetMapping(value = "/login", params = "failure")
-//	public String loginFail(Model model) {
-//		model.addAttribute("failureMessage", "ログインに失敗しました");
-//		// ログイン画面を表示
-//		return "login";
-//	}
+	// SecurityConfig の failureUrl で指定した URL と?のうしろのパラメータ
+	@GetMapping(value = "/login", params = "failure")
+	public String loginFail(Model model) {
+		model.addAttribute("failureMessage", "ログインに失敗しました");
+		// ログイン画面を表示
+		return "login";
+	}
 //	// SecurityConfig の defaultSuccessUrl で指定した URL
 //		@GetMapping("loginsuccess")
 //		public String loginSuccess(Model model) {
