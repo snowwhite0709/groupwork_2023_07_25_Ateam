5a3f1b159e03c94dae8507be0fd4a419e0926fb6
3030678 <3030678@30306-78>
2023-12-13 15:40:58 +0900

    Merge branch 'master' of https://github.com/ueda268/AttendanceManagement.git

--- src/main/java/com/example/attendanceManagement/controller/MainpageController.java
+++ src/main/java/com/example/attendanceManagement/controller/MainpageController.java
@@ -1,42 +1,24 @@
 package com.example.attendanceManagement.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.example.demo.form.CalcForm;
-import com.example.demo.validator.CalcValidator;
 
 @Controller
 public class MainpageController {
-	@ModelAttribute
-	public CalcForm setUpForm() {
-		return new CalcForm();
+	@GetMapping("/show")
+	public String sample(Model model) {
+		return "tanaka";
 	}
-	@GetMapping("show")
-	public String showView() {
-		return "entry";
-	}
-	@PostMapping("calc")
-	public String confirmView(@Validated CalcForm form,BindingResult bindingResult,Model model) {
-		if(bindingResult.hasErrors()) {
-			return "entry";
-		}
-		Integer result=form.getLeftNum()+form.getRightNum();
-		model.addAttribute("result",result);
-		return "confirm";
-	}
-	@Autowired
-	CalcValidator calcValidator;
-	@InitBinder("calcForm")
-	public void initBinder(WebDataBinder webDataBinder) {
-		webDataBinder.addValidators(calcValidator);
+	
+	@GetMapping("/attend")
+	public String attend() {
+		
+		System.out.println("abc");
+		Date date=new Date();
+		System.out.println(date);
+		return "redirect :/show";
 	}
 }	
