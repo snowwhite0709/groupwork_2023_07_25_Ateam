f3ee1597022e8ddd30959f26220af71572347b2a
3030821 <3030821@30308-21>
2023-12-14 11:10:57 +0900

    Workエンティティを更新しました。

--- src/main/java/com/example/attendanceManagement/entity/Work.java
+++ src/main/java/com/example/attendanceManagement/entity/Work.java
@@ -1,6 +1,8 @@
 package com.example.attendanceManagement.entity;
 
-import java.sql.Timestamp;
+import java.sql.Date;
+
+import org.springframework.data.annotation.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,12 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor  // デフォルトコンストラクターの自動生成
 @AllArgsConstructor // 全フィールドに対する初期化値を引数に取るコンストラクタを自動生成
 public class Work {
-	
+	 @Id
 	private Integer id;
-	private Timestamp day;
-	private Timestamp attendancetime;
-	private Timestamp leavingtime;
-	private Timestamp overtime;
-	private Integer kind;
-	private boolean approval;
+	private Integer employee_id;
+	private Date day;
+	private String attendancetime;
+	private String leavingtime;
+	private String overtime;
 }
