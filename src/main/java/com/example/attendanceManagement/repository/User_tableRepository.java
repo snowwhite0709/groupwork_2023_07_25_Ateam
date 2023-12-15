890a6004aabca63b5f63af291d5297e64955474c
3030823 <3030823@30308-23>
2023-12-15 13:07:12 +0900

    インサート文

--- src/main/java/com/example/attendanceManagement/repository/User_tableRepository.java
+++ src/main/java/com/example/attendanceManagement/repository/User_tableRepository.java
@@ -1,7 +1,9 @@
 package com.example.attendanceManagement.repository;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.attendanceManagement.entity.User_table;
 
@@ -11,4 +13,7 @@ public interface User_tableRepository extends CrudRepository<User_table, Integer
 	@Query("SELECT * FROM user_table ORDER BY id ASC")
 	Iterable<User_table> getAll();
 	
+	@Modifying
+    @Query("INSERT INTO login_user (username, password, role_id) VALUES (:username, :password, :roleId)")
+    void insertLoginUser(@Param("username") String username, @Param("password") String password, @Param("roleId") Integer roleId);
 }
