d41cca29c6c6d4900cbe6aba6b1826c159af8d7d
3030820 <3030820@30308-20>
2023-12-22 10:07:37 +0900

    paypay

---
+++ src/main/java/com/example/attendanceManagement/form/PaypayForm.java
@@ -0,0 +1,13 @@
+package com.example.attendanceManagement.form;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class PaypayForm {
+
+	private Integer employee_id;
+	private Integer basepay;
+	private Date day;
+}
